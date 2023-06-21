61f9c0585d9e60873bac577353a614c221619082
WaiYan <wphyo4662@gmail.com>
2023-06-21 15:56:43 +0630

    url mapping

---
+++ src/main/java/com/example/urlmappingdemo/controller/WelcomeController.java
@@ -0,0 +1,25 @@
+package com.example.urlmappingdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+    @GetMapping("/greeting")
+    public String greeting(Model model){
+        model.addAttribute("tagline", "Greeting From MVC");
+        return "welcome";
+    }
+    @GetMapping("/sayspring")
+    public String saySpring(Model model){
+        model.addAttribute("tagline", "Say Spring MVC");
+        return "welcome";
+    }
+    @GetMapping({"/", "/home"})
+    public String welcome(Model model){
+        model.addAttribute("tagline", "Welcome Spring MVC");
+        return "welcome";
+
+    }
+}
